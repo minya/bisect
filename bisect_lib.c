1e621c64d9a301dc18ba47b4fde58327d9a13d52
Mikhail Brel <m@brel.me>
2026-01-02 16:45:19 -0500

    Search fixed

--- bisect_lib.c
+++ bisect_lib.c
@@ -1,3 +1,4 @@
+#include <_string.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,27 +8,26 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "precise_time.h"
 #include "search_range.h"
 
 
-static size_t _BLOCK_SIZE = 4096;
+static size_t _BLOCK_SIZE = 8192;
 
-void printout(int fd, size_t position, size_t stop, precise_time_t start_time, precise_time_t end_time);
+void printout(int fd, size_t from, struct search_range_t range);
 
-size_t lower_bound(int fd, size_t file_size, struct search_range_t range, bool (*cmp)(precise_time_t, precise_time_t)) {
+
+size_t lower_bound_block(int fd, size_t file_size, struct search_range_t range, bool (*cmp)(precise_time_t, precise_time_t)) {
+    size_t n_blocks = file_size / _BLOCK_SIZE;
     size_t begin = 0;
-    size_t end = file_size;
+    size_t end = n_blocks;
 
+    char buffer[_BLOCK_SIZE];
+    char date_str[64];
     while (begin < end) {
-        if (end - begin <= _BLOCK_SIZE) {
-            break; // If the range is small enough, stop bisecting
-        }
         size_t mid = (begin + end) / 2;
 
-        lseek(fd, mid, SEEK_SET);
-        size_t remaining_size = end - mid;
-        size_t read_size = remaining_size < 1023 ? remaining_size : 1023;
-        char buffer[read_size + 1];
+        lseek(fd, mid * _BLOCK_SIZE, SEEK_SET);
         int bytes_read = read(fd, buffer, sizeof(buffer) - 1);
         if (bytes_read < 0) {
             perror("Error reading file");
@@ -38,11 +38,9 @@ size_t lower_bound(int fd, size_t file_size, struct search_range_t range, bool (
         buffer[bytes_read] = '\0';
         int date_offset_in_buf = find_date_in_buffer(buffer);
         if (date_offset_in_buf < 0) {
-            end = mid;
-            continue;
+            exit(EXIT_FAILURE);
         }
 
-        char date_str[40];  // Enough for timestamp with fractional seconds
         int date_len = extract_date_string(buffer, date_offset_in_buf, date_str, sizeof(date_str));
         if (date_len < 0) {
             end = mid;
@@ -51,18 +49,17 @@ size_t lower_bound(int fd, size_t file_size, struct search_range_t range, bool (
         
         precise_time_t found_time = string_to_precise_time(date_str);
 
-        if (cmp(range.start, found_time)) {
-            end = mid;
-        } else {
+        if (cmp(found_time, range.start)) {
             begin = mid + 1;
+        } else {
+            end = mid;
         }
     }
+    if (begin > 0)
+        --begin;
     return begin;
 }
 
-bool precise_not_greater(precise_time_t a, precise_time_t b) {
-    return !precise_greater(a, b);
-}
 
 void bisect(const char *filename, struct search_range_t range) {
     int fd = open(filename, O_RDONLY);
@@ -73,105 +70,80 @@ void bisect(const char *filename, struct search_range_t range) {
     size_t file_size = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
 
-    size_t begin = lower_bound(fd, file_size, range, precise_less);
-    size_t end = lower_bound(fd, file_size, range, precise_not_greater);
+    size_t first_block_with_date = lower_bound_block(fd, file_size, range, precise_less);
 
-    printf("Found range (hex) from %zx to %zx\n", begin, end);
-
-    if (begin >= file_size) {
+    if (first_block_with_date * _BLOCK_SIZE >= file_size) {
         close(fd);
         return;
     }
 
-    printout(fd, begin, end + _BLOCK_SIZE, range.start, range.end);
+    printout(fd, first_block_with_date * _BLOCK_SIZE, range);
     close(fd);
 }
 
-void printout(int fd, size_t from, size_t to, precise_time_t start_time, precise_time_t end_time) {
+void printout(int fd, size_t from, struct search_range_t range) {
+    /*printf("printout at offset %zu\n", from);*/
+    char buf[_BLOCK_SIZE+1];
     lseek(fd, from, SEEK_SET);
-    // Large enough buffer to read multiple lines.
-    // Needs to be replaced by a list of buffers or become dynamic as we do not know how large the lines are.
-    const size_t _BUF_SIZE = 8192;
-    char buffer[_BUF_SIZE+1]; // + 1 for null terminator
-    size_t remaining_bytes = 0;
-    size_t pos_to_print_from = SIZE_MAX;
-    while (true)
-    {
-        if (from >= to + _BUF_SIZE) {
-            break; // Stop reading if we have reached the end position
+    size_t rd = read(fd, buf, _BLOCK_SIZE);
+    buf[rd] = '\0';
+
+    int dt_offset = 0;
+    char date_str[64];
+    int date_len = 0;
+    precise_time_t date;
+
+    // Do not read more than 2 blocks to find the first date
+    unsigned int blocks_read = 1;
+
+    do {
+        int new_dt_offset_rel = find_date_in_buffer(buf + dt_offset + date_len);
+        if (new_dt_offset_rel < 0) {
+            if (blocks_read++ > 1) {
+                return;
+            }
+            size_t remainder_size = sizeof(buf) - 1 - dt_offset;
+            memcpy(buf, buf + dt_offset, remainder_size);
+            int rd = read(fd, buf + remainder_size, sizeof(buf) - remainder_size - 1);
+            if (rd == 0) {
+                return;
+            }
+            buf[remainder_size + rd] = '\0';
+            dt_offset = 0;
+            new_dt_offset_rel = 0;
+            continue;
         }
+        dt_offset += date_len + new_dt_offset_rel;
+        date_len = extract_date_string(buf, dt_offset, date_str, sizeof(date_str));
+        date = string_to_precise_time(date_str);
+    } while (precise_less(date, range.start));
 
-        int bytes_read = read(fd, buffer + remaining_bytes, sizeof(buffer) - 1 - remaining_bytes);
-        if (bytes_read < 0) {
-            perror("Error reading file");
-            return;
-        }
-        if (bytes_read == 0) {
-            break; // End of file reached
-        }
+    /*printf("first date found at %d\n", dt_offset);*/
+    if (dt_offset < 0)
+        return;
 
-        from += bytes_read;
-
-        buffer[bytes_read + remaining_bytes] = '\0';
-        size_t total_bytes_read = bytes_read + remaining_bytes;
-        remaining_bytes = 0;
-
-        char *buf_ptr = buffer;
-        while (buf_ptr < buffer + total_bytes_read) {
-            int found_time = find_date_in_buffer(buf_ptr);
-
-            if (found_time < 0) {
-                size_t copy_from = buf_ptr - buffer;
-                if (pos_to_print_from != SIZE_MAX) {
-                    // If we have a position to print from, we need to copy that part to the beginning of the buffer
-                    copy_from = pos_to_print_from;
-                    pos_to_print_from = 0;
-                }
-                memmove(buffer, buffer + copy_from, total_bytes_read - copy_from);
-                remaining_bytes = total_bytes_read - copy_from;
-
+    while (precise_less_equal(date, range.end)) {
+        int new_dt_offset_rel = find_date_in_buffer(buf + dt_offset + date_len);
+        /*printf("new date found at %d rel %d abs\n", new_dt_offset_rel, new_dt_offset_rel + dt_offset + date_len);*/
+        if (new_dt_offset_rel >= 0) {
+            int new_dt_offset = new_dt_offset_rel + dt_offset + date_len;
+            date_len = extract_date_string(buf, new_dt_offset, date_str, sizeof(date_str));
+            date = string_to_precise_time(date_str);
+            write(STDOUT_FILENO, buf + dt_offset, new_dt_offset - dt_offset);
+            dt_offset = new_dt_offset;
+        } else {
+            /*printf("hello\n");*/
+            size_t remainder_size = sizeof(buf) - 1 - dt_offset;
+            memcpy(buf, buf + dt_offset, remainder_size);
+            int rd = read(fd, buf + remainder_size, sizeof(buf) - remainder_size - 1);
+            buf[remainder_size + rd] = '\0';
+            dt_offset = find_date_in_buffer(buf + date_len);
+            if (dt_offset < 0 ) {
                 break;
             }
-
-            if (pos_to_print_from != SIZE_MAX) {
-                size_t pos_to_print_to = (buf_ptr - buffer) + found_time;
-                int n_bytes_to_print = pos_to_print_to - pos_to_print_from;
-                if (write(STDOUT_FILENO, buffer + pos_to_print_from, n_bytes_to_print) < 0) {
-                    perror("(2) Error writing to stdout");
-                    // printf("pos_to_print_from: %zu\n", pos_to_print_from);
-                    // printf("total_bytes_read: %zu\n", total_bytes_read);
-                    // printf("n_bytes_to_print: %d\n", n_bytes_to_print);
-                    // printf("found_time: %d\n", found_time);
-                    // printf("buf_ptr - buffer: %ld\n", (buf_ptr - buffer));
-                    return;
-                }
-                
-                pos_to_print_from = SIZE_MAX;
-            }
-
-            char date_str[40];
-            int date_len = extract_date_string(buf_ptr, found_time, date_str, sizeof(date_str));
-            if (date_len < 0) {
-                // If we can't extract the date, skip ahead conservatively
-                buf_ptr += found_time + 19;
-                continue;
-            }
-            
-            precise_time_t found_time_value = string_to_precise_time(date_str);
-            if (precise_less(found_time_value, start_time)) {
-                buf_ptr += found_time + date_len;
-            } else if (precise_greater(found_time_value, end_time)) {
-                break;
-            } else {
-                pos_to_print_from = (buf_ptr - buffer) + found_time;
-                buf_ptr += found_time + date_len;
-            }
-        }
-    }
-
-    if (pos_to_print_from != SIZE_MAX) {
-        if (write(STDOUT_FILENO, buffer + pos_to_print_from, remaining_bytes - pos_to_print_from) < 0) {
-            perror("(3) Error writing to stdout");
+            date_len = extract_date_string(buf, dt_offset, date_str, sizeof(date_str));
+            date = string_to_precise_time(date_str);
+            write(STDOUT_FILENO, buf, dt_offset);
         }
     }
 }
--- precise_time.c
+++ precise_time.c
@@ -82,6 +82,7 @@ int find_date_in_buffer(const char *buffer) {
 
     reti = regexec(&regex_datetime, buffer, 1, pmatch, 0);
     if (reti == 0) {
+        /*printf("DEBUG (find_date_in_buffer): strlen = %zu (found at %lld)\n", strlen(buffer), pmatch[0].rm_so);*/
         return pmatch[0].rm_so;
     }
     return -1;
@@ -90,6 +91,7 @@ int find_date_in_buffer(const char *buffer) {
 // Extract date string from buffer and return its length
 int extract_date_string(const char *buffer, int offset, char *date_str, size_t max_len) {
     regmatch_t pmatch[1];
+    /*printf("DEBUG: strlen = %zu (offset was %d)\n", strlen(buffer + offset), offset);*/
     int reti = regexec(&regex_datetime, buffer + offset, 1, pmatch, 0);
     if (reti == 0 && pmatch[0].rm_so == 0) {
         int len = pmatch[0].rm_eo - pmatch[0].rm_so;
